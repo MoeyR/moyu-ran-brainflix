a7f25d7486a78cfba25662dee3b3a72db78eb7a2
MoeyR <ranm@sheridancollege.ca>
2024-03-09 22:59:25 -0500

    Changed API path in brain-flix-api.js

--- src/utils/brain-flix-api.js
+++ src/utils/brain-flix-api.js
@@ -1,65 +1,44 @@
 import axios from "axios";
-const APIKEY = "d7ba838f-5663-4ea2-8d1d-29adfd09382d"
 
 class BrainFlixApi {
-    constructor(apikey){
-        this.baseUrl = "https://unit-3-project-api-0a5620414506.herokuapp.com";
-        this.apikey = apikey;
+  constructor() {
+    this.baseUrl = "http://localhost:8080";
+  }
+
+  //Get All Videos
+  async getVideos() {
+    try {
+      const response = await axios.get(
+        `${this.baseUrl}/videos`
+      );
+      return response.data;
+    } catch (error) {
+      console.log(`GET videos request failed, ${error}`);
     }
+  }
 
-    //Get All Videos
-    async getVideos(){
-        try {
-            const response = await axios.get(
-                `${this.baseUrl}/videos?api_key=${this.apikey}`
-            );
-            return response.data;
-        } catch (error) {
-            console.log(`GET videos request failed, ${error}`);
-        }
+  async getVideoDetails(id) {
+    try {
+      const response = await axios.get(`${this.baseUrl}/videos/${id}`);
+      return response.data;
+    } catch (error) {
+      console.log(`GET video details request failed, ${error}`);
     }
+  }
 
-    
-    async getVideoDetails(id){
-        try {
-            const response = await axios.get(
-                `${this.baseUrl}/videos/${id}`, 
-                {
-                    params: {
-                        "Content-Type": "application/json",
-                        "api_key": `${this.apikey}`
-                    }
-                }
-            );
-            return response.data;
-        } catch (error) {
-            console.log(`GET video details request failed, ${error}`);
-        }
+  async postComment(id, comment) {
+    try {
+        await axios.post(
+        `${this.baseUrl}/videos/${id}/comments`,
+        comment
+      );
+    } catch (error) {
+      console.log(`POST comment request failed, ${error}`);
     }
-
-
-    async postComment(id, comment){
-        try {
-            const res = await axios.post(
-                `${this.baseUrl}/videos/${id}/comments`, comment,
-                {
-                    params: {
-                        "Content-Type": "application/json",
-                        "api_key": `${this.apikey}`
-                    }
-                }
-            )
-            
-        } catch (error) {
-            console.log(`POST comment request failed, ${error}`);
-        }
-    }
-
-
+  }
 }
 
-
 //Instantiate BrainFlixApi
-const apiClient = new BrainFlixApi(APIKEY);
+const apiClient = new BrainFlixApi();
 
-export {apiClient}
+export { apiClient };
